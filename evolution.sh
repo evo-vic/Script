4dfa1c063610ae2149d9f2d1fb183da56c34167d
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2024-11-25 23:18:39 +0530

    Update evolution.sh

--- evolution.sh
+++ evolution.sh
@@ -8,15 +8,15 @@ repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 /opt/crave/resync.sh
 # cloning DT
 # device tree
-git clone https://github.com/EvolutionXA15/device_xiaomi_mojito.git --depth 1 -b mojito-universe device/xiaomi/mojito
-git clone https://github.com/EvolutionXA15/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito-universe device/xiaomi/sm6150-common
+git clone https://github.com/EvoXA15/device_xiaomi_mojito.git --depth 1 -b mojito-universe device/xiaomi/mojito
+git clone https://github.com/EvoXA15/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito-universe device/xiaomi/sm6150-common
 # kernel tree
-git clone https://github.com/EvolutionXA15/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
+git clone https://github.com/EvoXA15/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
 # vendor tree
 git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
 git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
 # hardware tree
-git clone https://github.com/EvolutionXA15/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
+git clone https://github.com/EvoXA15/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 # set build environment
 . build/envsetup.sh
 # lunch
